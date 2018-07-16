ed9e39109facb35087c09f11202f25d133509a78
luongnhuyen <luongnhuyen@gmail.com>
2018-07-16 22:56:15 +0700

    1

--- app.py
+++ app.py
@@ -9,43 +9,12 @@ mlab.connect()
 
 @app.route('/')
 def index():
-    if request.method == "GET":
-        return render_template('index.html')
-    elif request.method == "POST":
-        all_food = Service.objects(type = "food",price__lte = food*1000000/90).limit(6)
-
-        all_book = Service.objects(type = "book",price__lte = n2*1000000).limit(6)
-        all_luxury = Service.objects(type = "luxury",price__lte = n3*1000000).limit(6)
-        all_charity = Service.objects(type = "charity",price__lte = n4*1000000).limit(6)
-        all_invest = Service.objects(type = "invest",price__lte = n5*1000000).limit(6)
-        count_food = len(all_food)
-        count_book = len(all_book)
-        count_luxury = len(all_luxury)
-        count_charity = len(all_charity)
-        count_invest = len(all_invest)
-        return render_template('spending.html',
-                                food = food,
-                                go = go,
-                                life =life,
-                                other =other,
-                                book = book,
-                                training = training,
-                                all_food = all_food,
-                                all_book = all_book,
-                                all_luxury = all_luxury,
-                                all_charity = all_charity,
-                                all_invest = all_invest,
-                                count_food = count_food,
-                                count_book = count_book,
-                                count_luxury = count_luxury,
-                                count_charity = count_charity,
-                                count_invest = count_invest
-                                )    
+    return render_template('index.html')   
 
 
 @app.route('/ao')
 def ao():
-    return render_template('page/ao-item.html')
+    return render_template('page/ao.html')
 
 @app.route('/new-service-input', methods= ["GET", "POST"])
 def new_service_input():
